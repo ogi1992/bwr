7f8192e6e76300ac1ef382c978c9d3adae62bd79
ogi1992 <askovico@gmail.com>
2023-03-26 14:30:40 +0200

    Delete unnecessary topic config

--- bwr-robot/src/main/java/com/example/bwrrobot/configs/topics/BWRTopicConfig.java
+++ bwr-robot/src/main/java/com/example/bwrrobot/configs/topics/BWRTopicConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.KafkaAdmin;
 
 @Configuration
-public class TaskTopicConfig {
+public class BWRTopicConfig {
 
   @Value(value = "${kafka.bootstrap-servers}")
   private String bootstrapServers;
